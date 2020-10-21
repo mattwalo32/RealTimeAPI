0b764e4de7c8f03adaef942b1021213049b13c79
mlw6 <mattwalowski@gmail.com>
2020-10-21 15:53:19 -0500

    Move conn manager into internal package

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -1,25 +1,37 @@
 package server
 
+import (
+	"net"
+)
+
 type RealTimeServer struct {
+	// Configuration passed into server
+	config *RealTimeServerConfig
+
+	// TODO: Abstract UDP Manager
+
+	// Close channel to stop all goroutines
 	doneChan chan bool
 }
 
 type RealTimeServerConfig struct {
-
+	UDPListeningAddress string
+	TCPListeningAddress string
 }
 
-func NewServer() *RealTimeServer {
+func NewServer(config RealTimeServerConfig) *RealTimeServer {
 	server := &RealTimeServer {
+		config: &config,
 		doneChan: make(chan bool),
 	}
 
 	return server
 }
 
-func Start() {
-
+func (r *RealTimeServer) Start() {
+	// Abstract away UDP/TCP connections
 }
 
-func Stop() {
-
+func (r *RealTimeServer) Stop() {
+	close(r.doneChan)
 }
