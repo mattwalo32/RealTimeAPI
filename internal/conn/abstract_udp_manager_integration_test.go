963fa6c2c0f8ed6e2a67d8cfa2c9af25bb98c6f4
mlw6 <mattwalowski@gmail.com>
2020-10-21 16:54:22 -0500

    Write integration tests for UDP

---
+++ internal/conn/abstract_udp_manager_integration_test.go
@@ -0,0 +1,58 @@
+package conn_test
+
+import (
+	"net"
+	"bytes"
+	"testing"
+	"github.com/mattwalo32/RealTimeAPI/internal/conn"
+)
+
+var (
+	
+)
+
+func createUDPManager(address string) (chan conn.Message, *conn.AbstractUDPManager) {
+	receivingChan := make(chan conn.Message, 2)
+	config := conn.UDPManagerConfig {
+		ReceivingChan: receivingChan,
+		Address: address,
+	}
+
+	return receivingChan, conn.NewUDPManager(config)
+}
+
+func TestSendMessages(t *testing.T) {
+	clientAAddress := "localhost:9999"
+	clientBAddress := "localhost:9998"
+
+	_, managerA := createUDPManager(clientAAddress)
+	clientBReceivingChan, _ := createUDPManager(clientBAddress)
+
+	clientAUDPAddr,_ := net.ResolveUDPAddr("udp4", clientAAddress)
+	clientBUDPAddr,_ := net.ResolveUDPAddr("udp4", clientBAddress)
+
+	message_tests := [][]byte{
+		[]byte(""),
+		[]byte("Test"),
+		[]byte("Longer message with more substance!"),
+		[]byte("!@#$%&*()39283   add\t\tflkj \n !!/?"),
+	}
+
+	for _,testData := range message_tests {
+		msg := conn.Message{
+			Data: testData,
+			Address: *clientBUDPAddr,
+		}
+
+		managerA.SendMessage(msg)
+		response := <-clientBReceivingChan
+
+		if !bytes.Equal(response.Data, testData) {
+			t.Errorf("Expected message data (%v) is different from actual (%v)", testData, response.Data)
+		}
+
+		if response.Address.String() != clientAUDPAddr.String() {
+			t.Errorf("Expected message to be from %v, got: %v", clientAAddress, response.Address)
+		}
+	}
+}
