94516cd53fd6289ef09db901054fba4ed61980ce
mlw6 <mattwalowski@gmail.com>
2020-10-22 21:16:20 -0500

    Create timer package

---
+++ internal/timer/timer.go
@@ -0,0 +1,75 @@
+package timer
+
+import (
+	"sync"
+	"github.com/google/uuid"
+	"time"
+)
+
+type TimerCallback func(interface{})
+
+type Timer struct {
+	doneChan chan bool
+	eventMap map[uuid.UUID]*TimerEvent
+	lock sync.Mutex
+}
+
+type TimerEvent struct {
+	callback TimerCallback
+	capture interface{}
+}
+
+func NewTimer() *Timer {
+	timer := &Timer{
+		doneChan: make(chan bool),
+		eventMap: make(map[uuid.UUID]*TimerEvent),
+	}
+
+	return timer
+}
+
+func (timer *Timer) startTimerForEvent(eventID uuid.UUID, numRepeats int, timeoutMs int) {
+	for i:= 0; i < numRepeats; i++ {
+		select {
+		case <-timer.doneChan:
+			return
+		case <-time.After(time.Duration(timeoutMs) * time.Millisecond):
+			timer.lock.Lock()
+			defer timer.lock.Unlock()
+			
+			event,eventExists := timer.eventMap[eventID]
+			if !eventExists {
+				return
+			}
+
+			event.callback(event.capture)
+		}
+	}
+}
+
+func (timer *Timer) AddRepeatingEvent(callback TimerCallback, capture interface{}, timeoutMs int, numberRepeats int) uuid.UUID {
+	id := uuid.New()
+	event := &TimerEvent{
+		callback: callback,
+		capture: capture,
+	}
+
+	timer.lock.Lock()
+	defer timer.lock.Unlock()
+	timer.eventMap[id] = event
+
+	go timer.startTimerForEvent(id, numberRepeats, timeoutMs)
+	return id
+}
+
+func (timer *Timer) RemoveEvent(eventID uuid.UUID) {
+	timer.lock.Lock()
+	defer timer.lock.Unlock()
+	delete(timer.eventMap, eventID)
+}
+
+func (timer *Timer) Stop() {
+	close(timer.doneChan)
+}
+
+
