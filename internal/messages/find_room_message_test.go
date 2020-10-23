69e63a5e1f38d911f7573a6710a051f8e2ffd6af
mlw6 <mattwalowski@gmail.com>
2020-10-22 19:00:31 -0500

    Run formatter

--- internal/messages/find_room_message_test.go
+++ internal/messages/find_room_message_test.go
@@ -1,16 +1,16 @@
 package messages
 
 import (
+	"github.com/google/uuid"
 	"net"
 	"testing"
-	"github.com/google/uuid"
 )
 
 var testAddressA = net.UDPAddr{[]byte{127, 0, 0, 1}, 50, ""}
 var testAddressB = net.UDPAddr{[]byte{255, 1, 2, 28}, 0, "zone"}
 
 func TestFindRoom_Type(t *testing.T) {
-	msg := FindRoomMessage {}
+	msg := FindRoomMessage{}
 
 	if msg.GetMessageType() != MESSAGE_FIND_ROOM {
 		t.Errorf("Expected message type %v, got: %v", MESSAGE_FIND_ROOM, msg.GetMessageType())
@@ -19,12 +19,12 @@ func TestFindRoom_Type(t *testing.T) {
 
 func TestFindRoom_EncodeDecode(t *testing.T) {
 	msg := FindRoomMessage{
-		SourceAddr: testAddressA,
-		DestAddr: testAddressB,
-		UserID: uuid.New(),
+		SourceAddr:          testAddressA,
+		DestAddr:            testAddressB,
+		UserID:              uuid.New(),
 		ShouldStartWhenFull: true,
-		MinPlayers: 1,
-		MaxPlayers: 2,
+		MinPlayers:          1,
+		MaxPlayers:          2,
 	}
 
 	data, err := msg.Encode()
@@ -35,27 +35,27 @@ func TestFindRoom_EncodeDecode(t *testing.T) {
 	decodedMsg := FindRoomMessage{}
 	decodedMsg.Decode(data)
 
-	if (msg.SourceAddr.String() != decodedMsg.SourceAddr.String()) {
+	if msg.SourceAddr.String() != decodedMsg.SourceAddr.String() {
 		t.Error("Source addresses do not match")
 	}
 
-	if (msg.DestAddr.String() != decodedMsg.DestAddr.String()) {
+	if msg.DestAddr.String() != decodedMsg.DestAddr.String() {
 		t.Error("Destination addresses do not match")
 	}
 
-	if (msg.UserID != decodedMsg.UserID) {
+	if msg.UserID != decodedMsg.UserID {
 		t.Error("UserIDs do not match")
 	}
 
-	if (msg.ShouldStartWhenFull != decodedMsg.ShouldStartWhenFull) {
+	if msg.ShouldStartWhenFull != decodedMsg.ShouldStartWhenFull {
 		t.Error("ShouldStartWhenFull does not match")
 	}
 
-	if (msg.MinPlayers != decodedMsg.MinPlayers) {
+	if msg.MinPlayers != decodedMsg.MinPlayers {
 		t.Error("MinPlayers does not match")
 	}
 
-	if (msg.MaxPlayers != decodedMsg.MaxPlayers) {
+	if msg.MaxPlayers != decodedMsg.MaxPlayers {
 		t.Error("MaxPlayers does not match")
 	}
 }
