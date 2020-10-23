93b842490fe107501011257f7498d37198c55cc0
mlw6 <mattwalowski@gmail.com>
2020-10-22 19:03:22 -0500

    Test message handler

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	"net"
+	
 )
 
 type RealTimeServer struct {
---
+++ pkg/server/message_handler_test.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"testing"
+	"github.com/mattwalo32/RealTimeAPI/internal/messages"
+)
+
+func TestAssertConfigValid_TooShort(t *testing.T) {
+	config := &MessageHandlerConfig{
+		MessageReceivingChan: make(chan messages.Encodable, MIN_RECEIVING_CHAN_CAP-1),
+		Address:       "",
+	}
+
+	err := assertConfigValid(config)
+	if err == nil {
+		t.Errorf("Expected error, receiving chan capacity is too low")
+	}
+}
+
+func TestAssertConfigValid_Valid(t *testing.T) {
+	config := &MessageHandlerConfig{
+		MessageReceivingChan: make(chan messages.Encodable, MIN_RECEIVING_CHAN_CAP),
+		Address:       "",
+	}
+
+	err := assertConfigValid(config)
+	if err != nil {
+		t.Errorf("Error not expected")
+	}
+}
