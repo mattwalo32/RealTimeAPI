fc04a0b5ab60234aaf4de2ac1de9b3ac3d5e4801
mlw6 <mattwalowski@gmail.com>
2020-11-20 19:00:39 -0600

    Remove timer on acknowledge message

---
+++ pkg/server/message_processing.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"github.com/mattwalo32/RealTimeAPI/internal/messages"
+)
+
+func (handler *MessageHandler) processMessage(msg messages.Encodable) {
+	switch msg.GetMessageType() {
+	case messages.MESSAGE_ACKNOWLEDGE:
+		// TODO: Handle acknowledgment
+	case messages.MESSAGE_JOIN_SERVER:
+		// TODO: Send back uuid
+	default:
+		handler.config.MessageReceivingChan <- msg
+	}
+}
+
+func (handler *MessageHandler) processAcknowledge(msg messages.AcknowledgementMessage) {
+	handler.removeMessageTimer(msg)
+}
+
+func (handler *MessageHandler) removeMessageTimer(msg messages.AcknowledgementMessage) {
+	evtID, evtExists := handler.messageRetryEventIDs[msg.GetID()]
+	if !evtExists {
+		return
+	}
+
+	handler.timer.RemoveEvent(evtID)
+}
